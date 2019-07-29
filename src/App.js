f445f146edd6364dd5d6801aaf8ffab29a41a039
Qiu <757384114@qq.com>
2019-07-29 23:37:23 +1000

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React from 'react';
+import { Component } from 'react';
 import {Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
-import logo from './logo.svg';
+import { DISHES } from './shared/dishes';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
+//function App() {
+class App extends Component {
+ 
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+ 
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
 
-      </Navbar>
-      <Menu/>
-    </div>
-  );
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
