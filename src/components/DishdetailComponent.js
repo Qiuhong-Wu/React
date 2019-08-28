471252216a7b2a924fd1a4d98ec240a880942a1c
Qiu <757384114@qq.com>
2019-08-29 00:00:43 +1000

    Fetch Post Comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -26,7 +26,7 @@ import { baseUrl } from '../shared/baseUrl';
             );
     }
 
-    function RenderComments({comments, addComment, dishId }) {
+    function RenderComments({comments, postComment, dishId }) {
         if (comments != null) {   
             return(
                 <div className="col-12 col-md-5 m-1">
@@ -42,7 +42,7 @@ import { baseUrl } from '../shared/baseUrl';
                             );                               
                         })}                          
                     </ul>
-                    <CommentForm dishId={dishId} addComment={addComment} />
+                    <CommentForm dishId={dishId} postComment={postComment} />
                 </div>   
             );
         }
@@ -87,7 +87,7 @@ import { baseUrl } from '../shared/baseUrl';
                     <div className="row">                       
                             <RenderDish dish={props.dish} />                                         
                             <RenderComments comments={props.comments}
-                                addComment={props.addComment}
+                                postComment={props.postComment}
                                 dishId={props.dish.id}
                             />                           
                     </div>
@@ -124,7 +124,7 @@ import { baseUrl } from '../shared/baseUrl';
 
         handleSubmit(values) {
             this.toggleModal();         
-            this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+            this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
             console.log('Current State is: ' +  JSON.stringify(values));
             //alert('Current State is: ' +  JSON.stringify(values));
         }
